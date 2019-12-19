a5b3b5e10b65c0da4d6584b05ebc2095ebefe272
Olga Rafaliuk <rafaliuk.olga@gmail.com>
2019-12-19 21:45:48 +0300

    Enabled remote wed driver

--- src/test/java/by/epam/ta/driver/DriverSingleton.java
+++ src/test/java/by/epam/ta/driver/DriverSingleton.java
@@ -1,11 +1,12 @@
 package by.epam.ta.driver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverSingleton {
 
@@ -15,29 +16,25 @@ public class DriverSingleton {
     }
 
     public static WebDriver getDriver() {
+        DesiredCapabilities capabilities = null;
         if (null == driver) {
             switch (System.getProperty("browser")) {
                 case "firefox": {
-                    WebDriverManager.firefoxdriver().setup();
-                    FirefoxOptions firefoxOptions = new FirefoxOptions();
-                    firefoxOptions.setHeadless(true);
-                    firefoxOptions.addArguments("--start-maximized");
-                    firefoxOptions.addArguments("--width=2560");
-                    firefoxOptions.addArguments("--height=1440");
-                    driver = new FirefoxDriver(firefoxOptions);
+                    capabilities = DesiredCapabilities.firefox();
                     break;
                 }
                 default: {
-                    WebDriverManager.chromedriver().setup();
-                    ChromeOptions chromeOptions = new ChromeOptions();
-                    chromeOptions.setHeadless(true);
-                    chromeOptions.addArguments("--start-maximized");
-                    chromeOptions.addArguments("--window-size=2560,1440");
-                    driver = new ChromeDriver(chromeOptions);
+                    capabilities = DesiredCapabilities.chrome();
                 }
             }
         }
-        return driver;
+
+        try {
+            capabilities.setPlatform(Platform.WINDOWS);
+            return new RemoteWebDriver(new URL("http://192.168.8.3:4444/wd/hub"), capabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException();
+        }
     }
 
     public static void closeDriver() {
@@ -45,3 +42,15 @@ public class DriverSingleton {
         driver = null;
     }
 }
+
+/*
+ java -jar selenium-server-standalone-3.141.59.jar -role hub
+*/
+
+/*
+ java -Dwebdriver.chrome.driver=C:\Users\User\Downloads\chromedriver.exe -jar C:\Users\User\Downloads\selenium-server-standalone-3.141.59.jar  -role node -hub http://192.168.8.3:4444/grid/register -browser "browserName=chrome,platform=WINDOWS" -port 5557
+ */
+
+/*
+ java -Dwebdriver.gecko.driver=C:\Users\User\Downloads\geckodriver.exe -jar C:\Users\User\Downloads\selenium-server-standalone-3.141.59.jar -role node -hub http://192.168.8.3:4444/grid/register -browser "browserName=firefox,platform=WINDOWS" -port 5558
+ */
